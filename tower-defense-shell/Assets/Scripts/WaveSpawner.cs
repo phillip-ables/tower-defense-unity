cb2927613a108a737c7557a1237d6e1c0339e149
Phillip <phillipables1@gmail.com>
2018-03-03 13:33:00 -0500

    script: wavesoawner in gamemaster now has a enemyPrefab access and a countdown before it calls spawnwave function

--- tower-defense-shell/Assets/Scripts/WaveSpawner.cs
+++ tower-defense-shell/Assets/Scripts/WaveSpawner.cs
@@ -1,16 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
 public class WaveSpawner : MonoBehaviour {
+    //wavespawner is basic, use instantiate command pretty excessivly 
+    public Transform enemyPrefab; //this one single enemy that weve made
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float timeBetweenWaves = 7.5f;
+    private float countdown = 2f; //two seconds for first wave
+
+
+    void Update()
+    {
+        if (countdown <= 0)
+        {
+            SpawnWave();
+            countdown = timeBetweenWaves;  //time before spawn of wave
+        }
+        countdown -= Time.deltaTime; //decrease our countdown by one each second
+    }
+
+    void SpawnWave()
+    {
+        Debug.Log("Wave incoming");
+
+    }
+
 }
