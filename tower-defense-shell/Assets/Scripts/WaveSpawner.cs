1a0ec5b11fe4c59203ec3300c3e6c880373bba85
Phillip <phillipables1@gmail.com>
2018-03-03 13:49:33 -0500

    spawnWave is a corutine that calls spawnenemy and yeilds a half second for every value in wave number

--- tower-defense-shell/Assets/Scripts/WaveSpawner.cs
+++ tower-defense-shell/Assets/Scripts/WaveSpawner.cs
@@ -1,25 +1,38 @@
 ﻿using UnityEngine;
+using System.Collections;
+//wavespawner is basic, use instantiate command pretty excessivly 
 public class WaveSpawner : MonoBehaviour {
-    //wavespawner is basic, use instantiate command pretty excessivly 
     public Transform enemyPrefab; //this one single enemy that weve made
+    public Transform spawnPoint;
 
     public float timeBetweenWaves = 7.5f;
     private float countdown = 2f; //two seconds for first wave
 
+    private int waveNumber = 0;
 
     void Update()
     {
         if (countdown <= 0)
         {
-            SpawnWave();
+            StartCoroutine(SpawnWave());
             countdown = timeBetweenWaves;  //time before spawn of wave
         }
         countdown -= Time.deltaTime; //decrease our countdown by one each second
     }
 
-    void SpawnWave()
+    IEnumerator SpawnWave() // when we span a wave we want to span a group of enemies
     {
-        Debug.Log("Wave incoming");
+        waveNumber++;
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f); //yield for IEnumerator
+        }
+    }
+
+    void SpawnEnemy ()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
 
     }
 
