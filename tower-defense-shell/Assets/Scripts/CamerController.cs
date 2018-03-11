b632f9f3caf4fb7041db30e3b574a3048fd14561
Phillip <phillipables1@gmail.com>
2018-03-11 01:07:43 -0500

    clamped zoom function on mouse scroll input

--- tower-defense-shell/Assets/Scripts/CamerController.cs
+++ tower-defense-shell/Assets/Scripts/CamerController.cs
@@ -1,11 +1,22 @@
 ﻿using UnityEngine;
 
 public class CamerController : MonoBehaviour {
+    private bool doMovement = true;
+
     public float panSpeed = 30f;
     public float panBorderThickness = 10f;
 
+    public float scrollSpeed = 5f;
+    public float minY = 10f;
+    public float maxY = 80f;
+
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            doMovement = !doMovement;
+        if (!doMovement)
+            return;
+
         if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)//key down up, every key
         {
             transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
@@ -22,5 +33,15 @@ public class CamerController : MonoBehaviour {
         {
             transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
         }
+
+        // this is descreate, this is a boolean state
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        Vector3 pos = transform.position;
+
+        pos.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        
+        transform.position = pos;
     }
 }
