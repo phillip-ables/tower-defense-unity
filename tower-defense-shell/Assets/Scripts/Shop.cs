e0b13e406d221a95443e5ae02fac7d5d4161854e
Phillip <phillipables1@gmail.com>
2018-03-11 09:57:52 -0400

    shop script, turret purchase onclick added to the shop scripts Purchase Turret Button

---
+++ tower-defense-shell/Assets/Scripts/Shop.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Shop : MonoBehaviour {  
+
+    public void PurchaseStandardTurret ()  // called from our ui element becasue we have it public, build manager, turret, money, ui things (removing or closing down shop until you have placed a turret)
+    {
+        Debug.Log("Standard Turret Purchased");
+    }
+    public void PurchaseAnotherStandardTurret()  // called from our ui element becasue we have it public, build manager, turret, money, ui things (removing or closing down shop until you have placed a turret)
+    {
+        Debug.Log("Another Turret Purchased");
+    }
+
+}
